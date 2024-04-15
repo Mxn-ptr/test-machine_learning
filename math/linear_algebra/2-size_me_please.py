75d693e0f2cd8d3ac649cebaaf11d169e0f86612
Mxn-ptr <maxence0@hotmail.fr>
2024-04-15 17:56:07 +0200

    Fix requierements : no import

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
-import numpy as np
 
 
 def matrix_shape(matrix):
     """ Calculates the shape of a matrix """
-    return list(np.shape(matrix))
+    shape = []
+    while type(matrix) is list:
+        shape.append(len(matrix))
+        matrix = matrix[0]
+    return shape
