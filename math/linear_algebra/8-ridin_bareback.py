5740b3cdb6368921ef81860d43e30843d4f2baab
Mxn-ptr <maxence0@hotmail.fr>
2024-04-17 18:50:28 +0200

    Fix: Add a condition if mismatching

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -4,6 +4,8 @@
 
 def mat_mul(mat1, mat2):
     """ Performs matrix multiplication """
+    if len(mat1[0]) != len(mat2):
+        return None
     mult = []
     for i in range(len(mat1)):
         mult.append([])
