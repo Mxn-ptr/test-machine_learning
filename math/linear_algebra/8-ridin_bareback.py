d71ee4416e53b7ce42bb983ea432b9d4c035553c
Mxn-ptr <maxence0@hotmail.fr>
2024-04-17 18:49:15 +0200

    Fix loop k

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -9,7 +9,7 @@ def mat_mul(mat1, mat2):
         mult.append([])
         for j in range(len(mat2[0])):
             result = 0
-            for k in range(len(mat1) - 1):
+            for k in range(len(mat2)):
                 result += mat1[i][k] * mat2[k][j]
             mult[i].append(result)
     return mult
