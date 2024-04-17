a8221f209af347d405dd5b995651e37d068d9030
Mxn-ptr <maxence0@hotmail.fr>
2024-04-17 18:40:23 +0200

    Add task 8

---
+++ math/linear_algebra/8-ridin_bareback.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" Documented """
+
+
+def mat_mul(mat1, mat2):
+    """ Performs matrix multiplication """
+    mult = []
+    for i in range(len(mat1)):
+        mult.append([])
+        for j in range(len(mat2[0])):
+            result = 0
+            for k in range(len(mat1) - 1):
+                result += mat1[i][k] * mat2[k][j]
+            mult[i].append(result)
+    return mult
