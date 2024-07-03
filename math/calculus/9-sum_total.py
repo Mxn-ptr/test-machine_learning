ab4c59096febba2c8dabbe0585744406584662e9
Mxn-ptr <maxence0@hotmail.fr>
2024-07-03 17:25:06 +0200

    Fix validation

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -2,7 +2,7 @@
 
 def summation_i_squared(n):
     """ Calcule the sum of squared of i """
-    if type(n) is not int:
+    if type(n) is not int or n <= 0:
         sum = -1
     else:
         sum = (n * (n + 1) * (2 * n + 1)) / 6
