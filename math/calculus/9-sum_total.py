a9fe682d129bc7532c8f6daae65da21bf9b6f22a
Mxn-ptr <maxence0@hotmail.fr>
2024-07-03 17:21:25 +0200

    Add task 9

---
+++ math/calculus/9-sum_total.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+def summation_i_squared(n):
+    """ Calcule the sum of squared of i """
+    if type(n) is not int:
+        sum = -1
+    else:
+        sum = (n * (n + 1) * (2 * n + 1)) / 6
+    return int(sum)
