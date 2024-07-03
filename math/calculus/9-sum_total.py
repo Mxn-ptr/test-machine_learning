1f635ab36ac9116e8620124ca29ac5e48e1dbc27
Mxn-ptr <maxence0@hotmail.fr>
2024-07-03 17:26:45 +0200

    Return none when n not valid

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -3,7 +3,5 @@
 def summation_i_squared(n):
     """ Calcule the sum of squared of i """
     if type(n) is not int or n <= 0:
-        sum = -1
-    else:
-        sum = (n * (n + 1) * (2 * n + 1)) / 6
-    return int(sum)
+        return None
+    return int((n * (n + 1) * (2 * n + 1)) / 6)
