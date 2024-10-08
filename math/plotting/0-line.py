aab09b5cc8eaa24ca976fd2f3526e8c9e817ec36
Mxn-ptr <maxence0@hotmail.fr>
2024-10-08 16:29:52 +0200

    Fix pycodestyle and add doc

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
+""" 0-line """
 import numpy as np
 import matplotlib.pyplot as plt
 
-def line():
 
+def line():
+    """ Create a line graph """
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
 
