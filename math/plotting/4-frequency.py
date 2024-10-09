e8692bdcae2c69062794a4d99575af1d3c8e6ddd
Mxn-ptr <maxence0@hotmail.fr>
2024-10-09 15:49:31 +0200

    Fix task 4

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -10,11 +10,10 @@ def frequency():
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
 
-    x = list(range(0, 101, 10))
+    x = np.arange(0, 110, 10)
     plt.title('Project A')
     plt.xlabel('Grades')
     plt.ylabel('Number of Students')
     plt.xticks(x)
-    counts, bin_edges = np.histogram(student_grades, bins=x)
-    plt.stairs(counts, bin_edges, fill=True, color='black')
+    plt.hist(student_grades, bins=x, edgecolor='black')
     plt.show()
